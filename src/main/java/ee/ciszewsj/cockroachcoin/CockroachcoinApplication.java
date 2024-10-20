62ad97690cecfdcd650296a9ad27db60ce7c35f9
ciszewsj <jakubpiotrciszewski@gmail.com>
2024-10-20 12:35:20 +0200

    init

---
+++ src/main/java/ee/ciszewsj/cockroachcoin/CockroachcoinApplication.java
@@ -0,0 +1,13 @@
+package ee.ciszewsj.cockroachcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CockroachcoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CockroachcoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/ciszewsj/cockroachcoin/CockroachcoinApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.ciszewsj.cockroachcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CockroachcoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
