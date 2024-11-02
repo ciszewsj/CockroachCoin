048930becc802223aadf94cd8066a6a42cf1233d
ciszewsj <jakubpiotrciszewski@gmail.com>
2024-11-02 11:27:10 +0100

    Tailwind

---
+++ frontend/src/componenets/atoms/MainButton.tsx
@@ -0,0 +1,10 @@
+import {FC, MouseEventHandler} from "react";
+
+export const MainButton: FC<{
+    onClick?: MouseEventHandler | undefined,
+}> = ({onClick}) => {
+    return (
+        <>
+        </>
+    )
+}
